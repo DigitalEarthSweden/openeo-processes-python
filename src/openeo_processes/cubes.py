fa92e8931459c9cb1e712f2e4ad142f8c1aa57c1
ValentinaHutter <85164505+ValentinaHutter@users.noreply.github.com>
2022-01-19 11:29:59 +0100

    Handle time dimension in filter_labels

--- src/openeo_processes/cubes.py
+++ src/openeo_processes/cubes.py
@@ -1440,6 +1440,8 @@ class FilterLabels:
            A data cube with the same dimensions. The dimension properties (name, type, labels, reference system and
            resolution) remain unchanged, except that the given dimension has less (or the same) dimension labels.
         """
+        if dimension in ['time', 't', 'times']:
+            dimension = get_time_dimension_from_data(data, dimension)
         if dimension not in data.dims:
             raise DimensionNotAvailable("A dimension with the specified name does not exist.")
         labels = data[dimension].values
