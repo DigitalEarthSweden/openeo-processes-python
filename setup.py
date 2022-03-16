bd51cc06a37033c26f0918ad71529faa8fee5b3f
Lukas Weidenholzer <17790923+LukeWeidenwalker@users.noreply.github.com>
2022-03-16 12:58:29 +0100

    remove pyscaffold because it fails to parse tagnames (#104)

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@
     Learn more under: http://pyscaffold.org/
 """
 
-import sys
 from setuptools import setup
 
 # Add here console scripts and other entry points in ini-style format
@@ -20,12 +19,5 @@ entry_points = """
 """
 
 
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,          
-          use_pyscaffold=True)
-
-
 if __name__ == "__main__":
-    setup_package()
+    setup()
