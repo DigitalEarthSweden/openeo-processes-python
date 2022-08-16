0b1fc92c8e118675ea80c6b130d60ec9ddd851a0
Daniel Flemström <daniel.flemstrom@ri.se>
2022-08-16 07:27:30 +0200

    Bitwise or implementation and corresponding tests

--- src/openeo_processes/__init__.py
+++ src/openeo_processes/__init__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+from pkg_resources import DistributionNotFound, get_distribution
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = 'openeo_processes'
+    dist_name = "openeo_processes"
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = 'unknown'
+    __version__ = "unknown"
 
 
 from openeo_processes.arrays import *
+from openeo_processes.bitwise import *
 from openeo_processes.comparison import *
 from openeo_processes.cubes import *
 from openeo_processes.logic import *
